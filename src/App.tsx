048be8ac6542dd5354515b9503b5febbafdf45f2
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-05-22 11:29:24 +0400

    create Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
+// Components
+import Item from "./Item/Item";
 // @mui stuff
 import Drawer from "@mui/material/Drawer";
 import LinearProgress from "@mui/material/LinearProgress";
@@ -29,7 +31,24 @@ function App() {
 	);
 	console.log(data);
 
-	return <div className="App">start</div>;
+	const getTotalItems = () => null;
+	const handleAddToCart = (clickedItem: CartItemType) => null;
+	const handleRemoveFromCart = () => null;
+
+	if (isLoading) return <LinearProgress />;
+	if (error) return <div>Something went wrong...</div>;
+
+	return (
+		<Wrapper>
+			<Grid container spacing={3}>
+				{data?.map((item) => (
+					<Grid item key={item.id} xs={12} sm={4}>
+						<Item item={item} handleAddToCart={handleAddToCart} />
+					</Grid>
+				))}
+			</Grid>
+		</Wrapper>
+	);
 }
 
 export default App;
