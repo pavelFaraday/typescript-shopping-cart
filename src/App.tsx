8e9264dd435446acc306ee2109b18c9c5202e1d5
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-05-22 10:41:02 +0400

    fetch data form API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+// @mui stuff
+import Drawer from "@mui/material/Drawer";
+import LinearProgress from "@mui/material/LinearProgress";
+import Grid from "@mui/material/Grid";
+import Badge from "@mui/material/Badge";
+import addShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+// Styles
+import { Wrapper } from "./App.styles";
+// Types
+export type CartItemType = {
+	id: number;
+	category: string;
+	description: string;
+	image: string;
+	price: number;
+	title: string;
+	amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+	await (await fetch("https://fakestoreapi.com/products")).json();
+
 function App() {
-	return (
-		<div className="App">
-			<h1>Test</h1>
-		</div>
+	const { data, isLoading, error } = useQuery<CartItemType[]>(
+		"products",
+		getProducts
 	);
+	console.log(data);
+
+	return <div className="App">start</div>;
 }
 
 export default App;
